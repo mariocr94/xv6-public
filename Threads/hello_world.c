5762e7f335aed9084baaa4fbee83f5c67e935190
Mario Carrillo <marioacr94@gmail.com>
2017-10-17 11:17:04 -0500

    primer semaforo

--- Threads/hello_world.c
+++ Threads/hello_world.c
@@ -1,12 +1,24 @@
 #include <pthread.h>
 #include <stdio..h>
-#define NUM_THREADS		1000
+#include <unistd.h>
+#include <semaphore.h>
 
+#define NUM_THREADS 10
+
+long saldo;
+sem_t mutex;
 void *PrintHello(void *threadid){
 	long tid;
 	long lsaldo;
 	tid =(long)threadid;
 	printf("Hello World! its ya boi, thread #%ld!\n", tid);
+	sem_wait(&mutex);
+	lsaldo = saldo;
+	sleep(1);
+	lsaldo +=1;
+	saldo = lsaldo;
+	sem_post(&mutex);
+
 	pthread_exit(NULL);
 }
 
@@ -14,6 +26,8 @@ int main (int argc, char *argv[]){
 	pthread_t threads[NUM_THREADS];
 	int rc;
 	long t;
+	saldo = 0;
+	sem_init(&mutex,0,1);
 	for(t=0; t<NUM_THREADS; t++){
 		printf("In main: creating thread %ld\n", t);
 		rc = pthread_create(&threads[y], NULL, PrintHello, (void*)t);
@@ -23,6 +37,11 @@ int main (int argc, char *argv[]){
 		}
 	}
 
-	pthread_exit(NULL);
+	for(t=0; t<NUM_THREADS; t++){
+		pthread_join(threads[t], NULL);
+	}
 
+	printf("El valode del saldo es %ld\n", saldo);
+
+	pthread_exit(NULL);
 }
